1071cb3ddd9e76f872b2a69be349833cf140dd4f
Bellepink02 <111175828+Bellepink02@users.noreply.github.com>
2022-09-09 02:27:06 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
-*main-entry point
-*
-*Return:Always 0 (sucessful)
-*/
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return (0);
-
-}
+       printf("with proper grammar, but the outcome is a piece of art,\n");
+       return (0);
+}       
