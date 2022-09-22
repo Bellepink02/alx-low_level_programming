32290c6a299ac892a8dbe630846f20b1a2475413
Bellepink02 <111175828+Bellepink02@users.noreply.github.com>
2022-09-23 00:27:30 +0100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,23 +8,6 @@
 
 void print_number(int n)
 {
-<<<<<<< HEAD
-	  unsigned int n1;
-
-	    n1 = n;
-	
-      	    if (n < 0)
-	        {
-		    _putchar('-');
-		        n1 = -n;
-		  }
-
-	        if (n1 / 10 != 0)
-		  {
-		      print_number(n1 / 10);
-		 	}
-		  _putchar((n1 % 10) + '0');
-=======
   unsigned int n1;
 
   n1 = n;
@@ -40,5 +23,4 @@ void print_number(int n)
     print_number(n1 / 10);
   }
   _putchar((n1 % 10) + '0');
->>>>>>> 24ebb27a4b2097c2e9d603eace766d1dc30574f1
 }
